a5f3325f5d72cd77567152923bf0069d5f2198d6
sheetal-10 <sheetalp257@gmail.com>
2021-11-03 19:34:12 +0530

    Refactored: indentation

--- src/main/java/ProcessingBall.java
+++ src/main/java/ProcessingBall.java
@@ -3,7 +3,7 @@ import processing.core.PApplet;
 import java.util.Arrays;
 import java.util.List;
 
-public class ProcessingBall extends PApplet{
+public class ProcessingBall extends PApplet {
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
@@ -12,7 +12,7 @@ public class ProcessingBall extends PApplet{
     int SPEED_3 = 3;
     int SPEED_4 = 4;
 
-   List<Ball> balls;
+    List<Ball> balls;
 
     Ball ball1;
     Ball ball2;
@@ -24,21 +24,21 @@ public class ProcessingBall extends PApplet{
     }
 
     @Override
-    public void settings (){
+    public void settings() {
         size(WIDTH, HEIGHT);
     }
 
-    public void setup(){
-        ball1 = new Ball(0, HEIGHT/4, SPEED_1);
-        ball2 = new Ball(0, HEIGHT*2/4, SPEED_2);
-        ball3 = new Ball(0, HEIGHT*3/4, SPEED_3);
-        ball4 = new Ball(0, HEIGHT*4/4, SPEED_4);
+    public void setup() {
+        ball1 = new Ball(0, HEIGHT / 4, SPEED_1);
+        ball2 = new Ball(0, HEIGHT * 2 / 4, SPEED_2);
+        ball3 = new Ball(0, HEIGHT * 3 / 4, SPEED_3);
+        ball4 = new Ball(0, HEIGHT * 4 / 4, SPEED_4);
 
-       balls =  Arrays.asList(ball1, ball2, ball3, ball4);
+        balls = Arrays.asList(ball1, ball2, ball3, ball4);
     }
 
     @Override
-    public void draw(){
+    public void draw() {
         balls.forEach(Ball::move);
         balls.forEach(ball -> ball.draw(this));
 
